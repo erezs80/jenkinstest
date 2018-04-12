648b81166d93a737c4fdc8293705f3708924e871
Ubuntu <erezs@Master.3bi0b1mwepiurpbhzcnwex1mle.ax.internal.cloudapp.net>
2018-04-12 09:19:06 +0000

    change script6

--- test.sh
+++ test.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-echo "NODE_NAME = ${NODE_NAME}"
-echo "Initial_JOB_NAME = $1"
+set -e
+counter=`cat /var/lib/jenkins/counter.log`
+counter=$((counter+1))
+echo $counter > /var/lib/jenkins/counter.log
+if [ $counter -lt 3 ]; then
+	ca /tmp/test.txt
+else
+	echo "Initial_JOB_NAME = $1"
+	echo "0" > /var/lib/jenkins/counter.log
+fi
